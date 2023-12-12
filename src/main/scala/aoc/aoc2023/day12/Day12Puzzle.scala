3e923c8af85253139d108d381544ab071fe63339
balintbarki <bbalint01@gmail.com>
2023-12-12 21:17:33 +0100

    More reduction

--- src/main/scala/aoc/aoc2023/day12/Day12Puzzle.scala
+++ src/main/scala/aoc/aoc2023/day12/Day12Puzzle.scala
@@ -9,28 +9,76 @@ case object Day12Puzzle extends DailyPuzzle2023(12, "unknown") {
 
   val operationalCode = "O"
   val damagedCode = "X"
-  val unknownCode = "."
+  val unknownCode = "_"
 
   override def calculatePart1(lines: Seq[String]): String = calculateUnfolded(lines, 1)
 
   override def calculatePart2(lines: Seq[String]): String = calculateUnfolded(lines, 5)
 
 
-  private def getReducedInputs(lines: Seq[String], unfoldCnt: Int) =
+  private def getReducedInputs(lines: Seq[String], unfoldCnt: Int): Seq[(String, String, Seq[Int], Seq[Int])] =
     lines.map(line => {
       val parts = line.split(" ")
       val originalRecord = Seq.fill(unfoldCnt)(parts.head.trim).mkString("?")
       val originalGroups = Seq.fill(unfoldCnt)(parts.last).mkString(",")
         .trim.split(",")
         .map(_.toInt).toSeq
-
-      (originalRecord, originalRecord
-        .replaceAll("\\.+", operationalCode) // replace internal multiple operational springs with one
-        .replaceAll(s"^$operationalCode", "") // replace starting operational spring with empty
-        .replaceAll(s"$operationalCode$$", "") // replace ending operational spring with empty
+      val initiallyReduced = (Seq.fill(unfoldCnt)(parts.head.trim).mkString("?")
+        .replaceAll("\\.+", operationalCode) // replace multiple operational springs with one
         .replaceAll("#", damagedCode)
         .replaceAll("\\?", unknownCode),
         originalGroups)
+
+      var changed = true
+      var (workingRecord, workingGroups) = initiallyReduced
+
+      while (changed && workingGroups.nonEmpty) {
+        var reducedRecord = workingRecord
+        var reducedGroups = workingGroups
+
+        reducedRecord = reducedRecord
+          .replaceAll(s"$operationalCode+", operationalCode) // replace multiple operational springs with one
+          .replaceAll(s"^$operationalCode", "") // replace starting operational spring with empty
+          .replaceAll(s"$operationalCode$$", "") // replace ending operational spring with empty
+
+        val head = reducedGroups.head
+        val last = reducedGroups.last
+        val leftExactMatchRegex =
+          s"""^$damagedCode[$damagedCode$unknownCode]{${head - 1}}[$operationalCode$unknownCode].*"""
+            .r
+        val rightExactMatchRegex =
+          s""".*[$operationalCode$unknownCode][$damagedCode$unknownCode]{${last - 1}}$damagedCode$$""".r
+        val countedUnknownRegex = s"""^$unknownCode{$head}$$""".r
+        val allUnknownRegex = s"""^$unknownCode+$$""".r
+
+        reducedRecord match {
+          case leftExactMatchRegex()                              =>
+            reducedRecord = reducedRecord.drop(reducedGroups.head + 1)
+            reducedGroups = reducedGroups.drop(1)
+          case rightExactMatchRegex()                             =>
+            reducedRecord = reducedRecord.dropRight(reducedGroups.last + 1)
+            reducedGroups = reducedGroups.dropRight(1)
+          case countedUnknownRegex() if reducedGroups.length == 1 =>
+            reducedRecord = reducedRecord.drop(reducedGroups.head)
+            reducedGroups = reducedGroups.drop(1)
+          case allUnknownRegex() if reducedGroups.isEmpty         =>
+            reducedRecord = ""
+          case _                                                  =>
+        }
+
+        changed = (reducedRecord != workingRecord) || (reducedGroups != workingGroups)
+
+        if (changed) {
+          //println(s"$workingRecord -> $reducedRecord, $workingGroups -> $reducedGroups")
+          workingRecord = reducedRecord
+          workingGroups = reducedGroups
+        }
+      }
+
+      if (workingRecord.isEmpty)
+        require(workingGroups.isEmpty)
+
+      (originalRecord, workingRecord, originalGroups, workingGroups)
     })
 
   private def calculateUnfolded(lines: Seq[String], unfoldCnt: Int): String = {
@@ -40,39 +88,46 @@ case object Day12Puzzle extends DailyPuzzle2023(12, "unknown") {
     // Generate group strings
     // Determine number of extra operationals
     // Generate all possible combination and check if it matches with reduced input
-    reducedInputs.map { case (originalRecord, record, groups) =>
+    reducedInputs.map { case (originalRecord, record, originalGroups, groups) =>
+      println(s"OriginalRecord: $originalRecord, record: $record, groups: $originalGroups")
+      val cnt = if (groups.nonEmpty) {
+        val recordPattern = new Regex(record.replaceAll(s"$unknownCode", "."))
+        val minLength = groups.sum + groups.length - 1
+        val recordLength = record.length
+        val extraOperationalCnt: Int = recordLength - minLength
+        val locationsForOperational = groups.length + 1
 
-      val recordPattern = new Regex(record)
-      val minLength = groups.sum + groups.length - 1
-      val recordLength = record.length
-      val extraOperationalCnt: Int = recordLength - minLength
-      val locationsForOperational = groups.length + 1
+        // Determine each combination where "extraOperationalCnt" can be distributed to "locationsForOperation" locations
+        val combinations = if (extraOperationalCnt > 0)
+          Combinatory.combinations(extraOperationalCnt, locationsForOperational, 0)
+            .flatMap(_.permutations)
+        else
+          Seq(Seq.fill(locationsForOperational)(0))
 
-      // Determine each combination where "extraOperationalCnt" can be distributed to "locationsForOperation" locations
-      val combinations = if (extraOperationalCnt > 0)
-        Combinatory.combinations(extraOperationalCnt, locationsForOperational, 0)
-          .flatMap(_.permutations)
-      else
-        Seq(Seq.fill(locationsForOperational)(0))
+        val adjustedCombinations = combinations
+          .map(
+            combination => Seq(combination.head) ++ combination.tail.dropRight(1).map(_ + 1) ++ Seq(combination.last))
 
-      val adjustedCombinations = combinations
-        .map(combination => Seq(combination.head) ++ combination.tail.dropRight(1).map(_ + 1) ++ Seq(combination.last))
-
-      // Check for each combination if they can match with the reduced input
-      val results = adjustedCombinations.map(combination => {
-        combination.map(operationalCode * _)
-          .zipAll(groups.map(damagedCode * _), "", "")
-      })
-        .map(next => next.foldLeft("") { case (prev, (first, second)) =>
-          prev + first + second
+        // Check for each combination if they can match with the reduced input
+        val results = adjustedCombinations.map(combination => {
+          combination.map(operationalCode * _)
+            .zipAll(groups.map(damagedCode * _), "", "")
         })
+          .map(next => next.foldLeft("") { case (prev, (first, second)) =>
+            prev + first + second
+          })
 
-      val cnt = results.count(result => recordPattern.matches(result))
+        results.count(result => {
+          recordPattern.matches(result)
+        })
+      }
+      else {
+        1
+      }
 
-      println(s"Record: $originalRecord, groups: $groups, cnt: $cnt")
+      //println(s"Record: $originalRecord, groups: $originalGroups, cnt: $cnt")
 
       cnt
-
     }.sum.toString
   }
 }
--- src/test/scala/aoctest/aoc2023test/day12/TestDay12.scala
+++ src/test/scala/aoctest/aoc2023test/day12/TestDay12.scala
@@ -4,8 +4,15 @@ import aoc.aoc2023.day12.Day12Puzzle
 import aoctest.PuzzleTest
 import org.junit.{Assert, Ignore, Test}
 
+import java.nio.file.Paths
+
 @Ignore
 class TestDay12 extends PuzzleTest(Day12Puzzle) {
+
+  @Test
+  def testDay12Debug(): Unit = {
+    testPart1("19", Some(Paths.get(s"src/test/scala/aoctest/aoc2023test/day12/debugInput.txt")))
+  }
 
   @Test
   def testDay12Part1(): Unit = {

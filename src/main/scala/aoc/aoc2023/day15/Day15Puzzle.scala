bd755e09a3f4a3abc4e68961fa22715f5143dedd
balintbarki <bbalint01@gmail.com>
2023-12-15 22:16:14 +0100

    Day15 Part1 implemented

--- src/main/scala/aoc/aoc2023/day15/Day15Puzzle.scala
+++ src/main/scala/aoc/aoc2023/day15/Day15Puzzle.scala
@@ -2,11 +2,14 @@ package aoc.aoc2023.day15
 
 import aoc.aoc2023.DailyPuzzle2023
 
-case object Day15Puzzle extends DailyPuzzle2023(15, "unknown") {
-  override def calculatePart1(
-    lines: Seq[String]): String = ???
+case object Day15Puzzle extends DailyPuzzle2023(15, "Lens Library") {
 
-  override def calculatePart2(
-    lines: Seq[String]): String = ???
+  def hash(s: String): Int = s.foldLeft(0)((acc, c) => ((acc + c.toInt) * 17) % 256)
+
+  override def calculatePart1(lines: Seq[String]): String = {
+    lines.head.split(",").map(hash).sum.toString
+  }
+
+  override def calculatePart2(lines: Seq[String]): String = ???
 
 }
--- src/test/scala/aoctest/aoc2023test/day15/TestDay15.scala
+++ src/test/scala/aoctest/aoc2023test/day15/TestDay15.scala
@@ -4,24 +4,26 @@ import aoc.aoc2023.day15.Day15Puzzle
 import aoctest.PuzzleTest
 import org.junit.{Ignore, Test}
 
-@Ignore
+
 class TestDay15 extends PuzzleTest(Day15Puzzle) {
 
   @Test
   def testDay15Part1(): Unit = {
-    testPart1("0")
+    testPart1("1320")
   }
 
   @Test
   def testDay15Part1real(): Unit = {
-    testPart1Real("0")
+    testPart1Real("513172")
   }
 
+  @Ignore
   @Test
   def testDay15Part2(): Unit = {
     testPart2("0")
   }
 
+  @Ignore
   @Test
   def testDay15Part2real(): Unit = {
     testPart2Real("0")
